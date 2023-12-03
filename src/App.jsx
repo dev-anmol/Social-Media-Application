69c09480cacb2781ec0b34054b5a0e9e7e09d982
Anmol Thakur <anmolll.thakurrr@gmail.com>
2023-12-03 13:55:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import SideBar from "./Components/SideBar";
+import CreatePost from "./Components/CreatePost";
+import PostList from "./Components/PostList";
+import PostListProvider from "./store/post-list-store";
+function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
+
+  return (
+    <PostListProvider>
+      <div className="app-container">
+        <SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+        <div className="content">
+          <Header />
+          {selectedTab === "Home" ? <PostList /> : <CreatePost />}
+          <Footer />
+        </div>
+      </div>
+    </PostListProvider>
+  );
+}
+
+export default App;
